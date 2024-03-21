68f5df39ef35df6dab311ac6c871bd738441a7d7
M. Rashid <51082957+codewithmuh@users.noreply.github.com>
2024-03-21 22:20:04 +0500

    Update taskServices.js

--- Application-Code/frontend/src/services/taskServices.js
+++ Application-Code/frontend/src/services/taskServices.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiUrl =  "http://k8s-threetie-mainlb-1dd958d0ec-679101898.us-east-1.elb.amazonaws.com//api/tasks" //process.env.REACT_APP_BACKEND_URL //"http://localhost:8080/api/tasks";
+const apiUrl = process.env.REACT_APP_BACKEND_URL //"http://localhost:8080/api/tasks";
 console.log(apiUrl)
 export function getTasks() {
     return axios.get(apiUrl);
